4e053e3b5ab88a5ebc4a5c78ecda511cbd6445e3
zhushunsheng <503305196@qq.com>
2018-06-26 17:12:19 +0800

    + nav icon

--- src/components/sub-nav/subNav.vue
+++ src/components/sub-nav/subNav.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="in-sub-nav">
-        <div class="in-sub-title"  @click="isOpen=!isOpen">{{title}}</div>
+        <div class="in-sub-title" @click="isOpen=!isOpen">
+            <div class="in-sub-text">{{title}}</div>
+            <div class="in-sub-icon" :class="{'in-sub-icon-open':isOpen}"></div>
+        </div>
         <div class="in-sub-warp" :style="{height:isOpen?height+'px':'0px'}">
             <slot></slot>
         </div>
@@ -56,9 +59,35 @@ export default {
         height: 50px;
         line-height: 50px;
         cursor: pointer;
+        display: flex;
         color: @regularColor;
         &:hover{
             background: #EAEAEA;
+        }
+        >.in-sub-text{
+            flex: 1;
+            height: 50px;
+        }
+        >.in-sub-icon{
+            width: 50px;
+            height: 50px;
+            position: relative;
+            transition: all .3s;
+            &::after{
+                position: absolute;
+                left: 20px;
+                top: 18px;
+                display: block;
+                content: '';
+                width: 10px;
+                height: 10px;
+                border-right:1px solid #CCC;
+                border-bottom:1px solid #CCC;
+                transform: rotate(45deg);
+            }
+        }
+        >.in-sub-icon-open{
+             transform: rotate(180deg);
         }
     }
     >.in-sub-warp{
