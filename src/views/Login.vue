5ae165cefe7a9b071ff7e1e76239a1b6ff97bc1f
zhushunsheng <503305196@qq.com>
2018-06-29 15:47:30 +0800

    == login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,39 @@
 <template>
     <div class="page-login">
             <div class="form-login">
-                 <div class="form-item">
-                    <h2 class="form-title">in2 后台登录</h2>
-                </div>
-                <div class="form-item">
-                    <in-input placeholder="请输入邮箱"></in-input>
-                </div>
-                 <div class="form-item">
-                    <in-input type="password" placeholder="请输入密码"></in-input>
-                </div>
-                <div class="form-item">
-                    <in-button type="primary" style="width:100%;" @click="loginClick">登录</in-button>
-                </div>
+                <in-form>
+                    <in-form-item>
+                        <h2 class="form-title">in2 后台登录</h2>
+                    </in-form-item>
+                    <in-form-item>
+                        <in-input placeholder="请输入邮箱" v-model="form.email"></in-input>
+                    </in-form-item>
+                    <in-form-item>
+                        <in-input type="password" placeholder="请输入密码" v-model="form.password"></in-input>
+                    </in-form-item>
+                    <in-form-item>
+                        <in-button  type="primary" 
+                                    style="width:100%;" 
+                                    @click="loginClick"
+                                    :disabled="this.form.email.trim()==''||this.form.password.trim()==''">登录</in-button>
+                    </in-form-item>
+                </in-form>
             </div>
     </div>
 </template>
 <script>
 export default {
+    data(){
+        return {
+            form:{
+                email:'',
+                password:''
+            }
+        }
+    },
     methods:{
         loginClick(){
+            this.$message("登录成功");
             this.$router.push({
                 name:'demo'
             });
