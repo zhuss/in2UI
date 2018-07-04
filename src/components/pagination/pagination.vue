6360cb52180f8c034033f1306d5e8e953043fe50
zhushunsheng <503305196@qq.com>
2018-07-04 12:06:28 +0800

    + icon

--- src/components/pagination/pagination.vue
+++ src/components/pagination/pagination.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="in-pagination">
-        <div class="in-pagination-item in-pagination-disabled" v-show="current==1">＜</div>
-        <div class="in-pagination-item" v-show="current>1" @click="current--">＜</div>
+        <div class="in-pagination-item in-pagination-disabled" style="margin-left: 0" v-show="current==1"><i class="icon-arrow-left"></i></div>
+        <div class="in-pagination-item" v-show="current>1" style="margin-left: 0" @click="current--"><i class="icon-arrow-left"></i></div>
         <div class="in-pagination-item" :class="{'in-pagination-active':current==item}" v-for="item in pageArray" @click="current=item">{{item}}</div>
-        <div class="in-pagination-item in-pagination-disabled" v-show="current==pageCount">＞</div>
-        <div class="in-pagination-item"  v-show="current<pageCount" @click="current++">＞</div>
+        <div class="in-pagination-item in-pagination-disabled" v-show="current==pageCount"><i class="icon-arrow-right"></i></div>
+        <div class="in-pagination-item"  v-show="current<pageCount" @click="current++"><i class="icon-arrow-right"></i></div>
     </div>
 </template>
 <script>
@@ -76,6 +76,7 @@ export default {
         user-select: none;
         font-size: 14px;
         display: inline-block;
+        vertical-align: middle;
         padding: 0 10px;
         min-width: 40px;
         height: 40px;
@@ -86,9 +87,30 @@ export default {
         border-radius:2px; 
         color: @regularColor;
         cursor: pointer;
-    }
-    .in-pagination-item:nth-child(n-2){
         margin-left: 10px;
+        position: relative;
+        >.icon-arrow-left{
+            position: absolute;
+            display: block;
+            left: 16px;
+            top: 14px;
+            width: 10px;
+            height: 10px;
+            border-left:1px solid @regularColor;
+            border-bottom:1px solid @regularColor;
+            transform: rotate(45deg);
+        }
+         >.icon-arrow-right{
+            position: absolute;
+            display: block;
+            left: 12px;
+            top: 14px;
+            width: 10px;
+            height: 10px;
+            border-right:1px solid @regularColor;
+            border-bottom:1px solid @regularColor;
+            transform: rotate(-45deg);
+        }
     }
     .in-pagination-active{
         border: 1px solid @primaryColor;
@@ -99,6 +121,7 @@ export default {
         opacity: .6;
         cursor: not-allowed;
     }
+
 }
 </style>
 
