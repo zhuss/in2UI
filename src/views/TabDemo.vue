6fff73d1ddf462fb1c97492c4fcc208fbf3ca25e
zhushunsheng <503305196@qq.com>
2018-07-09 11:31:11 +0800

    代码格式

--- src/views/TabDemo.vue
+++ src/views/TabDemo.vue
@@ -1,29 +1,31 @@
 <template>
-    <div class="demo-block">
-        <h2 class="demo-title">Tab标签案例</h2>
-        <in-tab>
-            <in-tab-panel label="悬赏好创意">
-               <in-form :inline="true">
-                   <in-form-item label="单品测评" >
-                       <in-switch v-model="form.solo"></in-switch>
-                   </in-form-item>
-               </in-form>
-            </in-tab-panel>
-            <in-tab-panel label="悬赏高EMV">
-                <in-form>
-                   <in-form-item label="EMV门槛">
-                       <in-number v-model="form.emv" :min="50" :max="2000"></in-number>
-                   </in-form-item>
-               </in-form>
-            </in-tab-panel>
-            <in-tab-panel label="悬赏高排名">
+    <div>
+        <div class="demo-block" ref="demo">
+            <h2 class="demo-title">Tab标签案例</h2>
+            <in-tab>
+                <in-tab-panel label="悬赏好创意">
                 <in-form :inline="true">
-                   <in-form-item label="赏金总额">
-                       <in-number v-model="form.number" :min="50" :max="2000"></in-number>
-                   </in-form-item>
-               </in-form>
-            </in-tab-panel>
-        </in-tab>  
+                    <in-form-item label="单品测评" >
+                        <in-switch v-model="form.solo"></in-switch>
+                    </in-form-item>
+                </in-form>
+                </in-tab-panel>
+                <in-tab-panel label="悬赏高EMV">
+                    <in-form>
+                    <in-form-item label="EMV门槛">
+                        <in-number v-model="form.emv" :min="50" :max="2000"></in-number>
+                    </in-form-item>
+                </in-form>
+                </in-tab-panel>
+                <in-tab-panel label="悬赏高排名">
+                    <in-form :inline="true">
+                    <in-form-item label="赏金总额">
+                        <in-number v-model="form.number" :min="50" :max="2000"></in-number>
+                    </in-form-item>
+                </in-form>
+                </in-tab-panel>
+            </in-tab>  
+        </div>
     </div>
 </template>
 <script>
